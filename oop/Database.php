61d51f00aa1978378392e19e1472993aad9a0eb4
Yossef237002 <121687961+Yossef237002@users.noreply.github.com>
2024-07-17 14:48:01 +0300

    3nd commit

--- oop/Database.php
+++ oop/Database.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private static $servername = "localhost";
+    private static $servername = "localhost"; ///
     private static $username = "root";
     private static $password = "";
     private static $dbname = "users";
